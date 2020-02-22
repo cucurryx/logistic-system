51ce4d025b488728eb18bf71f552ac20639ad41b
xiebei <xiebei1108@outlook.com>
2020-02-22 18:23:55 +0800

    fix: add more detail error message in contract

--- chaincode/contract/logistic_contract.go
+++ chaincode/contract/logistic_contract.go
@@ -28,14 +28,13 @@ func (c *LogisticContract) Instantiate() {
 }
 
 // 订单创建接口
-func (c *LogisticContract) CreateOrder(ctx Context, requestData []byte) error {
+func (c *LogisticContract) CreateOrder(ctx Context, requestData string) error {
 	var request CreateOrderRequest
-	if err := json.Unmarshal(requestData, &request); err != nil {
+	if err := json.Unmarshal([]byte(requestData), &request); err != nil {
 		return fmt.Errorf("create order failed when unmarshal request data, err: %+v", err)
 	}
 
-	// this order shouldn't exist in world state
-	if _, err := ctx.GetStub().GetState(request.ID); err == nil {
+	if value, _ := ctx.GetStub().GetState(request.ID); value != nil {
 		return fmt.Errorf("orderer with goods id: %s has been created", request.ID)
 	}
 
@@ -51,20 +50,20 @@ func (c *LogisticContract) CreateOrder(ctx Context, requestData []byte) error {
 }
 
 // 运输信息录入接口
-func (c *LogisticContract) PutTransportInfo(ctx Context, requestData []byte) error {
+func (c *LogisticContract) PutTransportInfo(ctx Context, requestData string) error {
 	var request PutTransportInfoRequest
-	if err := json.Unmarshal(requestData, &request); err != nil {
+	if err := json.Unmarshal([]byte(requestData), &request); err != nil {
 		return fmt.Errorf("put transport info failed when unmarshal request data, err: %+v", err)
 	}
 
 	data, err := ctx.GetStub().GetState(request.GoodsID)
 	if err != nil {
-		return err
+		return fmt.Errorf("PutTransportInfo GetState failed with id: %s, err: %+v", request.GoodsID, err)
 	}
 
 	var goodsState GoodsState
 	if err := json.Unmarshal(data, &goodsState); err != nil {
-		return err
+		return fmt.Errorf("PutTransportInfo unmarshal goodsState failed, data: %+v, err: %+v", data, err)
 	}
 
 	if goodsState.State != UNPROCESSED && goodsState.State != WAREHOUSING {
@@ -82,20 +81,20 @@ func (c *LogisticContract) PutTransportInfo(ctx Context, requestData []byte) err
 }
 
 // 仓储信息录入接口
-func (c *LogisticContract) PutWarehouseInfo(ctx Context, requestData []byte) error {
+func (c *LogisticContract) PutWarehouseInfo(ctx Context, requestData string) error {
 	var request PutWarehouseInfoRequest
-	if err := json.Unmarshal(requestData, &request); err != nil {
+	if err := json.Unmarshal([]byte(requestData), &request); err != nil {
 		return fmt.Errorf("put warehouse info failed when unmarshal request data, err: %+v", err)
 	}
 
 	data, err := ctx.GetStub().GetState(request.GoodsID)
 	if err != nil {
-		return err
+		return fmt.Errorf("PutWarehouseInfo GetState failed with id: %s, err: %+v", request.GoodsID, err)
 	}
 
 	var goodsState GoodsState
 	if err := json.Unmarshal(data, &goodsState); err != nil {
-		return err
+		return fmt.Errorf("PutTransportInfo unmarshal failed, data: %+v, err: %+v", data, err)
 	}
 
 	if goodsState.State != TRANSPORTING {
@@ -136,35 +135,35 @@ func (c *LogisticContract) ReceiveGoods(ctx Context, id string) error {
 }
 
 // 获取物件当前状态
-func (c *LogisticContract) GetGoodsInfo(ctx Context, id string) (*GoodsState, error) {
+func (c *LogisticContract) GetGoodsInfo(ctx Context, id string) (GoodsState, error) {
 	data, err := ctx.GetStub().GetState(id)
 	if err != nil {
-		return nil, err
+		return GoodsState{}, fmt.Errorf("GetGoodsInfo GetState failed, id: %s, err: %+v", id, err)
 	}
 	var goodsState GoodsState
 	if err := json.Unmarshal(data, &goodsState); err != nil {
-		return nil, err
+		return GoodsState{}, fmt.Errorf("GetGoodsInfo unmarshal json failed, data: %+v, err: %+v", data, err)
 	}
-	return &goodsState, nil
+	return goodsState, nil
 }
 
 // 获取物件transaction记录
-func (c *LogisticContract) GetGoodsHistory(ctx Context, id string) (*HistoryResponse, error) {
+func (c *LogisticContract) GetGoodsHistory(ctx Context, id string) (HistoryResponse, error) {
 	histories, err := ctx.GetStub().GetHistoryForKey(id)
 	if err != nil {
-		return nil, err
+		return HistoryResponse{}, fmt.Errorf("GetGoodsHistory GetHistoryForKey failed, id: %s, err: %+v", id, err)
 	}
 
 	var response HistoryResponse
 	for histories.HasNext() {
 		history, err := histories.Next()
 		if err != nil {
-			return nil, err
+			return HistoryResponse{}, err
 		}
 
 		var goodsState GoodsState
 		if err := json.Unmarshal(history.GetValue(), &goodsState); err != nil {
-			return nil, err
+			return HistoryResponse{}, err
 		}
 
 		response.Transactions = append(response.Transactions,
@@ -173,7 +172,23 @@ func (c *LogisticContract) GetGoodsHistory(ctx Context, id string) (*HistoryResp
 				Timestamp: timestampToStr(history.GetTimestamp()),
 			})
 	}
-	return &response, nil
+	return response, nil
+}
+
+func (c *LogisticContract) GetState(ctx Context, key string) (string, error) {
+	value, err := ctx.GetStub().GetState(key)
+	if err != nil {
+		return "", fmt.Errorf("GetState error, err: %+v", err)
+	}
+	return string(value), nil
+}
+
+func (c* LogisticContract) PutState(ctx Context, key, value string) error {
+	if err := ctx.GetStub().PutState(key, []byte(value)); err != nil {
+		return fmt.Errorf("PutState error, err: %+v", err)
+	} else {
+		return nil
+	}
 }
 
 func timestampToStr(ts *timestamp.Timestamp) string {
