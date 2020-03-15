0c32e913dded1b2c95b6960d84ae991e805dc8c0
xiebei <xiebei1108@outlook.com>
2020-03-15 13:14:27 +0800

    feat: lua scripts used for benching

---
+++ bench/create_order.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body = "{\"id\": \"121212\",\"client\": \"client\",\"receiver\": \"接收者\",\"name\": \"物品名称\",\"source\": \"出发地\",\"destination\": \"接收地\",\"description\": \"aaa\"}"
